c720c696b90985fa16533b81e3bbc08bf3ab1d11
MansiPatil1400 <165757619+MansiPatil1400@users.noreply.github.com>
2026-08-23 21:39:29 +0530

    Add files via upload

---
+++ LB_Assignment_Day3/Sum.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Sum {
+
+    static int sumOfTwoNumbers(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+
+        int answer = sumOfTwoNumbers(num1, num2);
+
+        System.out.println("Sum = " + answer);
+    }
+}
