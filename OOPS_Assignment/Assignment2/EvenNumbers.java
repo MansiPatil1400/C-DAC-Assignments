7f472b2533e02a4e0fd6cf8142802d00c60aa3d3
MansiPatil1400 <165757619+MansiPatil1400@users.noreply.github.com>
2026-09-15 09:24:27 +0530

    Add files via upload

---
+++ OOPS_Assignment/Assignment2/EvenNumbers.java
@@ -0,0 +1,12 @@
+package NumberFractions;
+
+public class EvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 2; i <= 500; i = i + 2) {
+            System.out.println(i);
+        }
+	}
+
+}
---
+++ OOPS_Assignment/Assignment2/SeventhNumber.java
@@ -0,0 +1,13 @@
+package NumberFractions;
+
+public class SeventhNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 7; i <= 200; i = i + 7) {
+            System.out.println(i);
+        }
+
+	}
+
+}
