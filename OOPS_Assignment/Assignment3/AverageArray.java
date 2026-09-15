c6d47c26ebf54bac905c5a47e005674d541ea6fc
MansiPatil1400 <165757619+MansiPatil1400@users.noreply.github.com>
2026-09-15 09:26:53 +0530

    Add files via upload

---
+++ OOPS_Assignment/Assignment3/AverageArray.java
@@ -0,0 +1,33 @@
+package array;
+
+import java.util.Scanner;
+
+public class AverageArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+        }
+
+        double average = (double) sum / n;
+
+        System.out.println("Average = " + average);
+	}
+
+}
---
+++ OOPS_Assignment/Assignment3/SumArray.java
@@ -0,0 +1,28 @@
+package array;
+
+import java.util.Scanner;
+
+public class SumArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + arr[i];
+        }
+        System.out.println("Sum = " + sum);
+	}
+
+}
