564dc154857e6e2ab58ae0a4fdc7fa2b00a795f1
MansiPatil1400 <165757619+MansiPatil1400@users.noreply.github.com>
2026-09-19 15:28:23 +0530

    Add files via upload

---
+++ OOPS_Assignment/Assignment 4/Derived.java
@@ -0,0 +1,8 @@
+
+public class Derived extends Student {
+	
+	public void display() {
+		System.out.println("This is child class.");
+	}
+
+}
---
+++ OOPS_Assignment/Assignment 4/Student.java
@@ -0,0 +1,14 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is parent class.");
+	}
+
+	public void display() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
