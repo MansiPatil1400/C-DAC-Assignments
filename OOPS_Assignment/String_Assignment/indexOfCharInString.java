bf97561e9a39d79e55cf41e19590d3650e4cdc25
MansiPatil1400 <165757619+MansiPatil1400@users.noreply.github.com>
2026-09-21 12:53:50 +0530

    Add files via upload

---
+++ OOPS_Assignment/String_Assignment/indexOfCharInString.java
@@ -0,0 +1,29 @@
+package StringLiterals;
+
+public class indexOfCharInString {
+	public static void main(String args[]) {
+		String str = "The quick brown fox jumps over the lazy dog.";
+		for (int i = 0; i < str.length(); i++) {
+			System.out.print(str.charAt(i) + "-");
+			System.out.print(str.codePointAt(i) + " ");		
+		}
+		
+		System.out.println();
+
+		        for (char ch = 'a'; ch <= 'z'; ch++) {
+		            System.out.print(ch + " ");
+		        }
+
+		        System.out.println();
+
+		        for (char ch = 'a'; ch <= 'z'; ch++) {
+		            System.out.print("=");
+		        }
+
+		        System.out.println();
+
+		        for (char ch = 'a'; ch <= 'z'; ch++) {
+		            System.out.print(str.indexOf(ch) + " ");
+		}
+	}
+}
